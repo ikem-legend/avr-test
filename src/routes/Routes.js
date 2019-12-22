359927c130f942cc9af0216d00e148bd22676b96
Ikemefuna Obioha <ikemoobioha@gmail.com>
2019-12-22 16:21:22 +0100

    Initial deploy from the root in place of accidental deploy of\ src
    Initial deploy from the root in place of accidental deploy of src

---
+++ src/routes/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+
+const Routes = () => (
+    // rendering the router with layout
+    <BrowserRouter>
+        <Switch>
+            
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
