247fedb420789cbdf06ba0769d655925c3a7efaf
Ikemefuna Obioha <ikemoobioha@gmail.com>
2020-01-13 14:06:36 +0100

    Updated top end of roundups page

--- src/pages/transactions/index.js
+++ src/pages/transactions/index.js
@@ -1,10 +1,11 @@
 import React, {Component, Fragment} from 'react'
 import {Redirect} from 'react-router-dom'
-import {Row, Col, Form, Button} from 'reactstrap'
+import {Row, Col, Form, FormGroup, Input, CustomInput, Button, Progress} from 'reactstrap'
 
 import {isUserAuthenticated} from '../../helpers/authUtils'
 // import { getLoggedInUser, isUserAuthenticated } from '../../helpers/authUtils'
 import Loader from '../../components/Loader'
+import TopUp from '../../assets/images/topups.svg'
 
 import TransactionTables from './TransactionTables'
 // import RevenueChart from './RevenueChart'
@@ -56,20 +57,24 @@ class Transactions extends Component {
 				  	<Col sm={8}>
 				  		<div className="mb-1 mt-1 top-up">
                 <Row>
-                  <Col>Image</Col>
-                  <Col>
-                    <h4>Top Up</h4>
+                  <Col md={6}>
+                    <img src={TopUp} alt="Top-up" />
+                  </Col>
+                  <Col md={3}>
+                    <h6 className="top-heading">Top Up</h6>
                     <p>Top-Ups are an easy way to make one-time investments</p>
                   </Col>
-                  <Col>
+                  <Col md={3}>
                     <Form>
-                      <input
-                        type="text"
-                        name="roundup"
-                        placeholder="Enter Amount"
-                        value={roundup}
-                        onChange={this.updateValue}
-                      />
+                      <FormGroup>
+                        <Input
+                          type="text"
+                          name="roundup"
+                          placeholder="Enter Amount"
+                          value={roundup}
+                          onChange={this.updateValue}
+                        />
+                      </FormGroup>
                       <Button color="red" className="mt-1" sm>Invest Now</Button>
                     </Form>
                   </Col>
@@ -80,11 +85,51 @@ class Transactions extends Component {
 
           <Row>
             {/* milestone */}
+            <Col md={8}>
+              <Row>
+                <Col md={8}>
+                  <h4>Round-Up Milestone</h4>
+                </Col>
+                <Col md={4}>
+                  <FormGroup row>
+                    <span>Pause{' '}</span>
+                    <CustomInput
+                      type="switch"
+                      id="roundupsSwitch"
+                      name="roundupsSwitch"
+                      label="Resume"
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={8}>
+                  <Progress value="52" color="blue">$2.60 Roundup (52%)</Progress>
+                </Col>
+              </Row>
+            </Col>
   				  {/* multipliers */}
+            <Col md={4}>
+              <p>Multiply your roundup amount to accelerate your investments. Eg: $0.10 in round-ups will be $0.20 in 2x</p>
+              <Row>
+                <Col>
+                  <Button color="light-blue">1x</Button>
+                </Col>
+                <Col>
+                  <Button color="deep-blue">2x</Button>
+                </Col>
+                <Col>
+                  <Button color="light-blue">5x</Button>
+                </Col>
+                <Col>
+                  <Button color="light-blue">10x</Button>
+                </Col>
+              </Row>
+            </Col>
           </Row>
 
 					{/* table */}
-					<Row className="mb-4">
+					<Row className="mt-4 mb-4">
 						<TransactionTables />
 					</Row>
 				</div>
