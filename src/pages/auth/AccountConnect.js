86739efdcbb1fe918f1f52f45fddf5201230301f
Ikemefuna Obioha <ikemoobioha@gmail.com>
2020-02-03 15:25:49 +0100

    Changed to dev

--- src/pages/auth/AccountConnect.js
+++ src/pages/auth/AccountConnect.js
@@ -309,7 +309,7 @@ class AccountConnect extends Component {
                       <div className="mt-3">
                         <PlaidLink
                           clientName="Avenir app"
-                          env="sandbox"
+                          env="development"
                           product={['auth', 'transactions']}
                           publicKey="3c3d222fa56168931abed2dc785bc2"
                           onExit={this.handleOnExit}
